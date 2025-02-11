558f02b8306f76a1bfb62ed246923927619e54c2
admin <admin@APL-5CG0041HN2>
2025-02-11 08:21:45 +0530

    DSA Basics | BinarySearch | 1-3

---
+++ DSA_1/src/com/dsa/sl3_BinarySearch/Bs01_TargetSearch.java
@@ -0,0 +1,30 @@
+package com.dsa.sl3_BinarySearch;
+
+public class Bs01_TargetSearch {
+
+	public static void main(String[] args) {
+		int [] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+	}
+	
+	static int binarySearch(int[] arr, int target) {
+		int start = 0; int end = arr.length-1;
+			
+		while(start <= end) {
+			int mid = start + (end-start)/2;
+			if (target > arr[mid]) {
+				start = mid+1;
+			}else if (target < arr[mid]) {
+				end = mid - 1;
+			}else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+	
+
+}
---
+++ DSA_1/src/com/dsa/sl3_BinarySearch/Bs02_CeilingOfANumber.java
@@ -0,0 +1,29 @@
+package com.dsa.sl3_BinarySearch;
+
+public class Bs02_CeilingOfANumber {
+
+	public static void main(String[] args) {
+		int [] arr = {2, 3, 5, 9, 14, 16, 18};
+		System.out.println(findCeiling(arr, 15));
+	}
+	
+	static int findCeiling(int [] arr, int target) {
+		int start = 0, end = arr.length-1;
+		if(target > arr[end]) {
+			return -1;
+		}
+		while(start <= end) {
+			int mid = start + (end-start)/2;
+			if(target < arr[mid]) {
+				end = mid-1;
+			}else if(target > arr[mid]) {
+				start = mid+1;
+			}else {
+				return mid;
+			}
+		}
+		
+		return start;
+	}
+
+}
