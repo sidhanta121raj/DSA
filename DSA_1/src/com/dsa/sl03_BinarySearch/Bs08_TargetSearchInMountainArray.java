afd41d9aba83722c0d8d94631aa2eb3cd5fc2e92
sidhanta121raj <sidhanta121raj@gmail.com>
2025-02-12 13:03:10 +0530

    DSA | BS | 08

---
+++ DSA_1/src/com/dsa/sl03_BinarySearch/Bs08_TargetSearchInMountainArray.java
@@ -0,0 +1,22 @@
+package com.dsa.sl03_BinarySearch;
+
+//https://leetcode.com/problems/find-in-mountain-array/
+public class Bs08_TargetSearchInMountainArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	interface MountainArray {
+		      public int get(int index);
+		      public int length() ;
+	}
+	
+	public int findInMountainArray(int target, MountainArray mountainArr) {
+		int index = -1;
+		
+		return index;   
+    }
+
+}
