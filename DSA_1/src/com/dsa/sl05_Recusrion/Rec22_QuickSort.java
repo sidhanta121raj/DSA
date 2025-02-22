40fe6bfd3ad0a0713fef039ea59dbf0b58ed774b
sidhanta121raj <sidhanta121raj@gmail.com>
2025-02-22 10:55:04 +0530

    DSA | Recursion | 20-22

---
+++ DSA_1/src/com/dsa/sl05_Recusrion/Rec22_QuickSort.java
@@ -0,0 +1,45 @@
+package com.dsa.sl05_Recusrion;
+
+import java.util.Arrays;
+
+public class Rec22_QuickSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 1, 7, 9, 5 };
+		quickSort(arr, 0, arr.length - 1, "main");
+		System.out.println(Arrays.toString(arr));
+	}
+
+	static void quickSort(int[] arr, int start, int end, String calledFrom) {
+		System.out.println(
+				"CalledFrom:" + calledFrom + " Array=" + Arrays.toString(arr) + " Start=" + start + " End=" + end);
+		if (start >= end) {
+			return;
+		}
+
+		int i = start;
+		int j = end;
+		int pivot = start + (end - start) / 2;
+
+		while (i <= j) {
+			while (arr[i] < arr[pivot]) {
+				i++;
+			}
+			while (arr[j] > arr[pivot]) {
+				j--;
+			}
+
+			if (i <= j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+
+		quickSort(arr, start, j, "1st");
+		quickSort(arr, i, end, "2nd");
+	}
+
+}
