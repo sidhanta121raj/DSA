be828336ebf03a7ffeae557bcb544b073f7fb085
sidhanta121raj <sidhanta121raj@gmail.com>
2025-02-27 10:04:39 +0530

    DSA | Subsets | 02

--- DSA_1/src/com/dsa/subset/Sub02_SubsetsWithDuplicates.java
+++ DSA_1/src/com/dsa/subset/Sub02_SubsetsWithDuplicates.java
@@ -1,32 +1,41 @@
 package com.dsa.subset;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Sub01_Subsets {
+public class Sub02_SubsetsWithDuplicates {
 
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3};
+		int[] arr = {1, 2, 2};
 		System.out.println(findSubSets(arr));
 	}
 	
 	static List<List<Integer>> findSubSets(int[] arr){
+		Arrays.sort(arr);
 		List<List<Integer>> results = new ArrayList<List<Integer>>();
 		
 		results.add(new ArrayList<>());
-		
-		for(int i: arr) {
+		int start = 0;
+		int end = 0;
+		for(int i = 0; i< arr.length; i++) {
+			start = 0;
+			if(i>0 && arr[i]==arr[i-1]) {
+				start = end + 1;
+			}
+			end = results.size()-1;
+			
 			System.out.println("i="+i);
 			int n = results.size();
 			System.out.println("n="+n);
-			for(int j =0; j<n; j++) {
+			for(int j = start; j<n; j++) {
 				System.out.println("	j="+j);
 				List<Integer> subsets = new ArrayList<Integer>(results.get(j));
-				System.out.println("	subsets="+subsets);
-				subsets.add(i);
-				System.out.println("	subsets="+subsets);
+				System.out.println("		subsets="+subsets);
+				subsets.add(arr[i]);
+				System.out.println("		subsets="+subsets);
 				results.add(subsets);
-				System.out.println("	Results="+results);
+				System.out.println("		Results="+results);
 			}
 		}
 		return results;
